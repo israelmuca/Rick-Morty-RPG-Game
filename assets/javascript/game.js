2d2b774b06e71fb3ca0831fb001c3ae34b1a54f6
IsraelMuCa <isra.munoz@gmail.com>
2018-05-14 00:04:53 -0500

    Now JS makes sure that you can't change the chracters midgame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,23 @@
 - Put chosen character in your character area
 - Put not chosen characters in enemies available to attack area
 - Listen to clicks in enemies available to attack area
+
 - Put chosen character in defender area
+
+- Listen for an attack
+- ¿Defender has HP?
+    - YES
+        - user'sChoice attack is deducted from defender's HP
+        - defender's counter is deducted from user'sChoice HP
+        - ¿user'sChoice has HP?
+            - YES
+                - user'sChoice attack is increased (++)
+            - NO
+                - Gameover
+
+    - NO
+        - Remove Defender from defender section
+        - Listen to clicks in enemies available to attack area
 
 */
 
@@ -46,17 +62,31 @@ $(document).ready(function() {
         htmlYourEnemiesSection.append(htmlCharactersContainer); //Moves the rest of the choices to the "Your Enemies" section
         //Change the CSS of the enemies
 
-        htmlYourEnemiesSection.click(function(event){
-            defenderCharacter = event.target.id; //Gets the enemy's name
-            defenderCharacterButton = event.target; //Get's the enemy's container (button)
+        //function to capture the click event and send it to the selectDEfender function as a parameter
+        //Has to be called from an anonymous function to be able to send the click event properly
+        htmlYourEnemiesSection.click(function(event){selectDefender(event);});
+    });
 
+    //Function called once all characters have been chosen
+    function startGame() {
+        //use button.dataset.hp to get HP from the character being used
+        //Listen to the attack click
+        $("#attack").click(function(){
+            //if 
+        });
+
+    }
+
+    //Function called to select defender
+    function selectDefender(pDefenderCharacterButton){
+        defenderCharacter = pDefenderCharacterButton.target.id; //Gets the enemy's name
+        defenderCharacterButton = pDefenderCharacterButton.target; //Get's the enemy's container (button)
+
+        if (htmlDefender.contents().length == 0) { //Makes sure only one character is "defender" at a time
             htmlDefender.append(defenderCharacterButton); //Moves the chosen enemy to "Defender" section
             //Change the CSS of the current defender
             startGame(); //Once all choosings have been done start the game
-        });
-    });
-
-    function startGame() {
+        }
 
     }
 } );
