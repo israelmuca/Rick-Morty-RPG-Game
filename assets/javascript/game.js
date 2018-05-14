a2e9f0c4f188e1110818aa568f6ab6fa1b8bd83d
IsraelMuCa <isra.munoz@gmail.com>
2018-05-14 01:37:11 -0500

    Finished the logic of the game, missing a proper UI and the stats in the button's padding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,8 @@
 - Listen to button click, to know which character was choosen
 - Put chosen character in your character area
 - Put not chosen characters in enemies available to attack area
-- Listen to clicks in enemies available to attack area
 
+- Listen to clicks in enemies available to attack area
 - Put chosen character in defender area
 
 - Listen for an attack
@@ -24,6 +24,10 @@
         - Remove Defender from defender section
         - Listen to clicks in enemies available to attack area
 
+
+TO DO:
+- Get a proper UI
+- Add the stats to the padding of the buttons
 */
 
 //Global variables
@@ -32,6 +36,9 @@ var userCharacter;
 var userCharacterButton;
 var defenderCharacter;
 var defenderCharacterButton;
+var defenderStats;
+var userStats;
+var statsAssigned = false;
 
 //Make sure the document is loaded, then...
 $(document).ready(function() {
@@ -44,6 +51,8 @@ $(document).ready(function() {
     var htmlFightSection = $("#fight-section");
     var htmlDefenderSection = $("#defender-section");
     var htmlDefender = $("#defender");
+    var lostSection = $("#lost-section");
+    var wonSection = $("#won-section");
     var htmlCharactersContainer = $("#characters-container");
     var htmlRick = $("#rick");
     var htmlMorty = $("#morty");
@@ -51,8 +60,10 @@ $(document).ready(function() {
     var htmlMeeseks = $("#meeseeks");
 
     htmlChooseCharacterButtonHolder.append(htmlCharactersContainer);
+    lostSection.hide();
+    wonSection.hide();
 
-    //Listens to the user choosing it's character
+    //Listens to the user choosing it's character, then calls the defender select function
     htmlChooseCharacterButtonHolder.click(function(event){
         userCharacter = event.target.id; //Gets the character's name
         userCharacterButton = event.target; //Gets the character's container (button)
@@ -67,26 +78,71 @@ $(document).ready(function() {
         htmlYourEnemiesSection.click(function(event){selectDefender(event);});
     });
 
-    //Function called once all characters have been chosen
-    function startGame() {
-        //use button.dataset.hp to get HP from the character being used
-        //Listen to the attack click
-        $("#attack").click(function(){
-            //if 
-        });
-
-    }
-
     //Function called to select defender
     function selectDefender(pDefenderCharacterButton){
-        defenderCharacter = pDefenderCharacterButton.target.id; //Gets the enemy's name
-        defenderCharacterButton = pDefenderCharacterButton.target; //Get's the enemy's container (button)
-
         if (htmlDefender.contents().length == 0) { //Makes sure only one character is "defender" at a time
+            defenderCharacter = pDefenderCharacterButton.target.id; //Gets the enemy's name
+            defenderCharacterButton = pDefenderCharacterButton.target; //Get's the enemy's container (button)
             htmlDefender.append(defenderCharacterButton); //Moves the chosen enemy to "Defender" section
             //Change the CSS of the current defender
-            startGame(); //Once all choosings have been done start the game
+            assignStats(); //Once both the user and defender have been chosen assignStats
         }
-
     }
-} );
+
+    //Function called once characters have been chosen
+    function assignStats() {
+        defenderStats = defenderCharacterButton.dataset;
+        userStats = userCharacterButton.dataset;
+        statsAssigned = true;
+    }
+
+    function gameOverLost() {
+        lostSection.show();
+    }
+
+    function gameOverWin() {
+        wonSection.show();
+    }
+
+    //Listen to the attack click
+    $("#attack").click(function(){
+        //Make sure that stats have been assigned
+        if(statsAssigned) {
+            var defenderHP = parseInt(defenderStats.hp);
+            var defenderCounterAttack = parseInt(defenderStats.counterAttack);
+            var userHP = parseInt(userStats.hp);
+            var userAttack = parseInt(userStats.attack);
+            if(defenderHP > 0){
+                if(userHP > 0) {
+                    //Do the math for the attack
+                    defenderHP -= userAttack;
+                    userHP -= defenderCounterAttack;
+                    userAttack = (userAttack*.25) + userAttack;
+                    //Save the new values to the HTML dataset
+                    defenderStats.hp = defenderHP;
+                    userStats.hp = userHP;
+                    userStats.attack = userAttack;
+                } else {
+                    gameOverLost();
+                }
+            } else {
+                if(htmlYourEnemiesSection.contents().children().length == 0) { //if there's no more enemies, you won!
+                    gameOverWin();
+                } else { //if there's defenders, bring the next one!
+                    //Once defender rans out of HP, remove it
+                    defenderCharacter = "";
+                    defenderCharacterButton = "";
+                    htmlDefender.empty();
+                    statsAssigned = false;
+                }
+            }
+        } else {
+            alert("First choose your players!")
+        }
+    });
+
+    //Reloads webpage when the user wants to play again
+    $(".restart").click(function(){
+        location.reload();
+    });
+});
